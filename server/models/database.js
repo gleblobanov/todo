79ab69620f0f10ac4138cf52685a50ff96503fb1
Gleb <mail@gleblobanov.ru>
2015-06-29 09:57:06 +0200

    Change database creation script.

--- server/models/database.js
+++ server/models/database.js
@@ -5,50 +5,41 @@ var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 
 var client = new pg.Client(connectionString);
 client.connect();
-//var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+//var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN), date TIMESTAMP WITH TIME ZONE');
+//var query = client.query('CREATE INDEX items_id_index ON items (id);');
 
-//query.on('end', function() { client.end(); });
+query.on('end', function() { client.end(); });
 
-var i = 0;
-var j = 0;
-
-function my_q() {
-
-    if ( i < 10000000) {
-        i++;
-        j++;
-        if (j == 1000000) {
-
-            console.log(j);
-            j = 0;
-        }
-
-        if (i % 2 === 0) {
-            client.query("INSERT INTO items (text, complete, date) VALUES ('task', true, '2015-01-08 19:25:00+02' );", [], function(err, result) {
-                my_q();
-            });
-        }
-        else {
-            client.query("INSERT INTO items (text, complete, date) VALUES ('task', false, '2015-01-08 19:25:00+02' );", [], function(err, result) {
-                my_q();
-            });
-        }
-    }
-    else {
-        client.end();
-    }
-}
-
-my_q();
-
-
+//var i = 0;
+//var j = 0;
 //
-//client.connect(function(err) {
-// for (var i = 0; i < 100; ++i) {
-// client.query("INSERT INTO items (text, complete) VALUES ('task', false );", [], function(err, result) {
+//function my_q() {
 //
-// });
+//    if ( i < 10000000) {
+//        i++;
+//        j++;
+//        if (j == 1000000) {
 //
+//            console.log(j);
+//            j = 0;
+//        }
+//
+//        if (i % 2 === 0) {
+//            client.query("INSERT INTO items (text, complete, date) VALUES ('task', true, '2015-01-08 19:25:00+02' );", [], function(err, result) {
+//                my_q();
+//            });
+//        }
+//        else {
+//            client.query("INSERT INTO items (text, complete, date) VALUES ('task', false, '2015-01-08 19:25:00+02' );", [], function(err, result) {
+//                my_q();
+//            });
+//        }
+//    }
+//    else {
+//        client.end();
+//    }
 //}
 //
-//});
+//my_q();
+
+
